dc27d99d40778ee3e2cc38c79730f6be01674730
jvican <jorgevc@fastmail.es>
2017-04-06 00:17:25 +0200

    Add -p to mkdir

--- bin/setup_cache.sh
+++ bin/setup_cache.sh
@@ -9,7 +9,7 @@ test -d "$IVY_CACHE" || \
   echo "Ivy cache has been set up: $IVY_CACHE.")
 
 export COURSIER_CACHE="$HOME/.coursier"
-mkdir "$DRONE_DIR/.coursier"
+mkdir -p "$DRONE_DIR/.coursier"
 test -d "$COURSIER_CACHE" || \
   test -h "$COURSIER_CACHE" || \
   mkdir "$DRONE_DIR/.coursier" || \
