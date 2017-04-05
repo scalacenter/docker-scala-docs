7b17d891acf8dbef0ab3bb40323c13838344b1a3
jvican <jorgevc@fastmail.es>
2017-04-05 22:22:44 +0200

    Fix order of arguments in ln

--- bin/setup_cache.sh
+++ bin/setup_cache.sh
@@ -3,10 +3,10 @@ set -eu
 
 export IVY_CACHE="$HOME/.ivy2"
 test -d "$IVY_CACHE" || \
-  ln "$IVY_CACHE" "$DRONE_DIR/.ivy2" && \
+  ln "$DRONE_DIR/.ivy2" "$IVY_CACHE" && \
   echo "Ivy cache has been set up: $IVY_CACHE."
 
-export COURSIER_CACHE="$DRONE_DIR/.coursier"
+export COURSIER_CACHE="$HOME/.coursier"
 test -d "$COURSIER_CACHE" || \
-  ln "$COURSIER_CACHE" "$DRONE_DIR/.coursier"  && \
+  ln "$DRONE_DIR/.coursier" "$COURSIER_CACHE"  && \
   echo "Coursier cache context has been set up: $COURSIER_CACHE."
