b6c51a64ffcfd17182252c4ce4b36bfa4f692f66
jvican <jorgevc@fastmail.es>
2017-04-12 15:26:41 +0200

    Modify store credentials

--- bin/store_credentials.sh
+++ bin/store_credentials.sh
@@ -2,26 +2,27 @@
 
 set +u
 
-if [[ -z "$DRONE_PULL_REQUEST" ]]; then
-  BINTRAY_USERNAME="CI_MOCKUP_PULL_REQUEST"
-  BINTRAY_PASSWORD="fake password"
-fi
+if [[ -z "$BINTRAY_USERNAME" && -z "$BINTRAY_PASSWORD" ]]; then
+  set -eu
 
-set -eu
+  BINTRAY_FOLDER="$HOME/.bintray"
+  CREDENTIALS_FILE="$BINTRAY_FOLDER/.credentials"
 
-BINTRAY_FOLDER="$HOME/.bintray"
-CREDENTIALS_FILE="$BINTRAY_FOLDER/.credentials"
-
-create_bintray_file() {
-  mkdir -p "$BINTRAY_FOLDER"
-cat <<EOF >> "$CREDENTIALS_FILE"
+  create_bintray_file() {
+    mkdir -p "$BINTRAY_FOLDER"
+  cat <<EOF >> "$CREDENTIALS_FILE"
 realm = Bintray API Realm
 host = api.bintray.com
 user = $BINTRAY_USERNAME
 password = $BINTRAY_PASSWORD
 EOF
-  echo "Created $CREDENTIALS_FILE."
-}
+    echo "Created $CREDENTIALS_FILE."
+  }
 
-([[ -e "CREDENTIALS_FILE" ]] && echo "Skipped creation of bintray credentials. $CREDENTIALS_FILE exists.") \
-  || create_bintray_file
+  ([[ -e "CREDENTIALS_FILE" ]] && echo "Skipped creation of bintray credentials. $CREDENTIALS_FILE exists.") \
+    || create_bintray_file
+
+else
+  echo "Skipped creation of bintray credentials file. Missing \$BINTRAY_USERNAME and \$BINTRAY_PASSWORD."
+fi
+
