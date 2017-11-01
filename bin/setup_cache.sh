c036312e5329052b6ddfedab9fa524ebcf067dd7
jvican <jorgevc@fastmail.es>
2017-11-01 20:48:03 +0100

    Clarify messages

--- bin/setup_cache.sh
+++ bin/setup_cache.sh
@@ -6,18 +6,18 @@ mkdir -p "$DRONE_DIR/.ivy2"
 test -d "$IVY_CACHE" || \
   test -h "$IVY_CACHE" || \
   (ln -s "$DRONE_DIR/.ivy2" "$IVY_CACHE" && \
-  echo "Ivy cache has been set up: $IVY_CACHE.")
+  echo "Ivy cache has been set up for $IVY_CACHE.")
 
 export COURSIER_CACHE="$HOME/.coursier"
 mkdir -p "$DRONE_DIR/.coursier"
 test -d "$COURSIER_CACHE" || \
   test -h "$COURSIER_CACHE" || \
   (ln -s "$DRONE_DIR/.coursier" "$COURSIER_CACHE"  && \
-  echo "Coursier cache context has been set up: $COURSIER_CACHE.")
+  echo "Coursier cache context has been set up for $COURSIER_CACHE.")
 
 export SBT_HOME_DIR="$HOME/.sbt"
 mkdir -p "$DRONE_DIR/.sbt"
 test -d "$SBT_HOME_DIR" || \
   test -h "$SBT_HOME_DIR" || \
   (ln -s "$DRONE_DIR/.sbt" "$SBT_HOME_DIR"  && \
-  echo "Sbt cache context has been set up: $SBT_HOME_DIR.")
+  echo "Sbt cache context has been set up for $SBT_HOME_DIR.")
